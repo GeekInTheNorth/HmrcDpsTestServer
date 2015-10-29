0299bf6319a36c34cac64a702c2767a545609981
Mark Stott <mypantsareontheoutside@gmail.com>
2015-10-29 15:40:24 +0000

    Add Clear Cache controller for clearing cached identities

---
+++ TestProxy/TestProxy/Controllers/ClearCacheController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace TestProxy.Controllers
+{
+    public class ClearCacheController : HmrcProxyControllerBase
+    {
+        [AcceptVerbs("POST","GET")]
+        public HttpResponseMessage ClearCache()
+        {
+            IdentityCache.Identities.Clear();
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent("The Identity Cache for Altered Messages has now been cleared.", Encoding.UTF8)
+            };
+        }
+    }
+}
