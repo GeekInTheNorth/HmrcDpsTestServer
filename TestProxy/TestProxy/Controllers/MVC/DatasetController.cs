4a3611be21a7430ad222a421f06581ca75e4a029
Mark Stott <mypantsareontheoutside@gmail.com>
2017-01-20 10:35:29 +0000

    Create Dataset Controller

---
+++ TestProxy/TestProxy/Controllers/MVC/DatasetController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using HmrcTpvsProxy.DAL.Repositories;
+using HmrcTpvsProxy.Domain.Datasets;
+using TestProxy.Models.Dataset;
+
+namespace TestProxy.Controllers.MVC
+{
+    public class DatasetController : Controller
+    {
+        private readonly IDatasetService service;
+
+        public DatasetController()
+        {
+            service = new DatasetService(new DatasetRepository());
+        }
+
+        // GET: Dataset
+        public ActionResult Index()
+        {
+            var summaries = service.GetDatasetSummaries();
+            var model = new DatasetModel(summaries);
+
+            return View(model);
+        }
+    }
+}
---
+++ TestProxy/TestProxy/Models/Dataset/DatasetModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HmrcTpvsProxy.Domain.Datasets;
+
+namespace TestProxy.Models.Dataset
+{
+    public class DatasetModel
+    {
+        public IEnumerable<DatasetSummary> Summaries { get; private set; }
+
+        public IEnumerable<string> MessageTypes { get; private set; }
+
+        public DatasetModel(IEnumerable<DatasetSummary> summaries)
+        {
+            Summaries = summaries;
+            MessageTypes = summaries.SelectMany(x => x.MessageCounts.Select(y => y.Key)).Distinct();
+        }
+    }
+}
