f165ee3e6680a42ac810833c4f4d6efed399f6ac
Mark Stott <mypantsareontheoutside@gmail.com>
2017-02-06 08:21:27 +0000

    Create Upload screen to allow users to bulk replace data

--- TestProxy/TestProxy/Controllers/MVC/DatasetController.cs
+++ TestProxy/TestProxy/Controllers/MVC/DatasetController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using HmrcTpvsProxy.DAL.Repositories;
 using HmrcTpvsProxy.Domain.Datasets;
 using HmrcTpvsProxy.Domain.Validators;
@@ -15,7 +18,6 @@ namespace TestProxy.Controllers.MVC
             service = new DatasetService(new DatasetRepository(), new PayeReferenceValidator());
         }
 
-        // GET: Dataset
         public ActionResult Index()
         {
             var apiPath = Request.Url
@@ -44,6 +46,36 @@ namespace TestProxy.Controllers.MVC
             model.ShowValidationError = true;
 
             return View(model);
+        }
+
+        public ActionResult Upload(int? id)
+        {
+            if (!id.HasValue) return RedirectToAction("Index");
+
+            var datasetBeingModified = service.GetDatasetSummaries().FirstOrDefault(x => x.Id == id.Value);
+
+            if (datasetBeingModified == null) return RedirectToAction("Index");
+
+            var model = new DatasetUploadModel { DatasetBeingModified = datasetBeingModified.Name };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(int? id, DatasetUploadModel model, HttpPostedFileBase file)
+        {
+            var fileName = file.FileName;
+
+            if (!fileName.EndsWith(".csv", StringComparison.CurrentCultureIgnoreCase))
+            {
+                model.ShowValidationError = true;
+                model.ValidationError = "Only CSV files should be uploaded.";
+                model.DatasetBeingModified = service.GetDatasetSummaries().FirstOrDefault(x => x.Id == id.Value).Name;
+
+                return View(model);
+            }
+
+            return Upload(id);
         }
     }
 }
---
+++ TestProxy/TestProxy/Models/Dataset/DatasetUploadModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using HmrcTpvsProxy.Domain;
+
+namespace TestProxy.Models.Dataset
+{
+    public class DatasetUploadModel
+    {
+        public string DatasetBeingModified { get; set; }
+
+        public string MessageType { get; set; }
+
+        public bool OverwriteExistingRecords { get; set; }
+
+        public List<SelectListItem> MessageTypes { get; set; }
+
+        public string ValidationError { get; set; }
+
+        public bool ShowValidationError { get; set; }
+
+        public DatasetUploadModel()
+        {
+            MessageTypes = new List<SelectListItem>();
+            MessageTypes.Add(GetSelectListItem(RequestType.P6));
+            MessageTypes.Add(GetSelectListItem(RequestType.P9));
+            MessageTypes.Add(GetSelectListItem(RequestType.SL1));
+            MessageTypes.Add(GetSelectListItem(RequestType.SL2));
+
+            MessageType = RequestType.P6.ToString();
+
+            ValidationError = string.Empty;
+            ShowValidationError = false;
+        }
+
+        private SelectListItem GetSelectListItem(RequestType requestType)
+        {
+            return new SelectListItem
+            {
+                Text = requestType.ToString(),
+                Value = requestType.ToString()
+            };
+        }
+    }
+}
